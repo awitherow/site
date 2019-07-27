aaed398e05709f13add191817f42397ac367886d
awitherow <au.witherow@gmail.com>
2019-07-26 17:17:41 -1000

    further prettify, add social icons in footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./index.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 function Footer() {
   return (
     <footer>
       <ul>
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
+        <li>
+          <a href="" target="_blank">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </li>
       </ul>
     </footer>
   );
