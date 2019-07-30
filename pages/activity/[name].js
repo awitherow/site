fe97643f3d3234dc7ec93a140d3181ed4294764a
awitherow <au.witherow@gmail.com>
2019-07-29 17:58:54 -1000

    implement faux filter bar

--- pages/activity/[name].js
+++ pages/activity/[name].js
@@ -1,6 +1,13 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { Button, Card, CardColumns } from "react-bootstrap";
+import {
+  ButtonToolbar,
+  ButtonGroup,
+  Button,
+  Card,
+  CardColumns,
+  Dropdown,
+} from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
@@ -39,8 +46,41 @@ const ActivityPage = () => {
       ) : null}
       {globalState.activity.products ? (
         <div className="products">
-          <h3>Products</h3>
+          <h3>Essential Items</h3>
           <Divider />
+          <h4>Filter items by...</h4>
+          <ButtonToolbar id="filter-bar">
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-type">
+                Type
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="dropdown-tag">
+                Tag
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <ButtonGroup aria-label="Cost range">
+              <Button variant="success">$</Button>
+              <Button variant="success">$$$</Button>
+              <Button variant="success">$$$</Button>
+            </ButtonGroup>
+          </ButtonToolbar>
+
+          <h5>{globalState.activity.products.length} Results</h5>
           <CardColumns>
             {globalState.activity.products.map(
               ({ _id, image, description, link, name, creator, tags }) => (
