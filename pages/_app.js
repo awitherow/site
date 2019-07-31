d778284703f01f8676d69153e1fda3813c4ad7de
awitherow <au.witherow@gmail.com>
2019-07-30 21:52:09 -1000

    add page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,47 @@
 import React, { useEffect } from "react";
 import App, { Container } from "next/app";
-import Router from "next/router";
+import { useRouter } from "next/router";
 
-import useGlobal from "../src/store";
+import { PageTransition } from "next-page-transitions";
 
 import "./_custom.scss";
 
-export default class HiVibe extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+export function HiVibe({ Component, pageProps }) {
+  const router = useRouter();
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <PageTransition timeout={300} classNames="page-transition">
+        <Component {...pageProps} key={router.route} />
+      </PageTransition>
+      <style jsx global>{`
+        .page-transition-enter {
+          opacity: 0;
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transition: opacity 300ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity 300ms;
+        }
+      `}</style>
+    </Container>
+  );
 }
+
+HiVibe.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
+
+export default HiVibe;
