d3d3d34ea8d2e22c36b9e4507a9f968a74b3ea2b
awitherow <au.witherow@gmail.com>
2019-08-02 17:04:57 -1000

    add book as editable interface via props

--- pages/mail/index.js
+++ pages/mail/index.js
@@ -6,33 +6,25 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 import "./index.scss";
 
-function Mail({ seo }) {
+function Mail({ seo, book }) {
   return (
     <Layout seo={seo} id="mail">
       <div className="content">
         <img src="https://via.placeholder.com/500" />
         <div className="preform&form">
           <div className="preform">
-            <h2>High Vibrational Habits</h2>
-            <p>
-              High Vibrational Habits is a quick dive into 5 Essential Habits
-              that should constitute your day, boiled down to their Essence, for
-              you to absorb and integrate, so that you can Live at your Highest
-              Vibration. Product recommendations are also within to help you get
-              vibe higher, faster.
-            </p>
+            <h2>{book.title}</h2>
+            <p>{book.description}</p>
             <h3>Table of Contents</h3>
             <ul>
-              <li>Starting Each Day Right</li>
-              <li>Biofuel for Your Rocket Ship</li>
-              <li>Wheels Up for a Sustainable Flight</li>
-              <li>Wheels Down for a Smooth Landing </li>
-              <li>Catalyzing Your Chances for a Successful Tomorrow</li>
+              {book.chapters.map(chapter => (
+                <li>{chapter}</li>
+              ))}
             </ul>
-            <h3>Get Your eBook Today!</h3>
-            <Divider />
           </div>
           <div className="form">
+            <h3>Get Your eBook Today!</h3>
+            <Divider />
             <MailchimpSubscribe url="https://awitherow.us19.list-manage.com/subscribe?u=c60ea29dfa8a09bdd8e0cca02&id=3f3895ba76" />
           </div>
         </div>
@@ -43,6 +35,18 @@ function Mail({ seo }) {
 
 Mail.getInitialProps = async () => {
   return {
+    book: {
+      title: "High Vibrational Habits",
+      description:
+        "High Vibrational Habits is a quick dive into 5 Essential Habits that should constitute your day, boiled down to their Essence, for you to absorb and integrate, so that you can Live at your Highest Vibration. Product recommendations are also within to help you get vibe higher, faster.",
+      chapters: [
+        "Starting Each Day Right",
+        "Biofuel for Your Rocket Ship",
+        "Wheels Up for a Sustainable Flight",
+        "Wheels Down for a Smooth Landing",
+        "Catalyzing Your Chances for a Successful Tomorrow",
+      ],
+    },
     seo: {
       title: "Free eBook From hivib.es | High Vibrational Habits",
       description:
