6f313c606f47043bf8c2c60c6925149f536691c0
awitherow <au.witherow@gmail.com>
2019-08-04 17:46:43 -1000

    proper implementation of sitemap and robots

---
+++ server/postBuild.js
@@ -0,0 +1,53 @@
+const fs = require("fs-extra");
+const sm = require("sitemap");
+const sanity = require("./sanity");
+const directory = "public";
+
+function buildRobotsTxt() {
+  const robotsTxt = `User-agent: *
+    Sitemap: https://hivib.es/sitemap.xml
+    Disallow:`;
+
+  fs.writeFileSync(`${directory}/robots.txt`, robotsTxt);
+  console.log("- Robots.txt saved!");
+}
+
+// Sitemap
+async function buildSitemap() {
+  const sitemap = sm.createSitemap({
+    hostname: "https://hivib.es",
+    cacheTime: 600000, // 600 sec - cache purge period
+  });
+
+  sitemap.add({
+    url: "/",
+    changefreq: "daily",
+    priority: 1,
+  });
+
+  const Items = await sanity.fetch(`
+    *[_type == 'hobby']{
+        name,
+    }`);
+
+  for (let i = 0; i < Items.length; i += 1) {
+    const item = Items[i];
+    sitemap.add({
+      url: `/activity/${item.name.replace(/\s+/g, "-").toLowerCase()}`,
+      changefreq: "daily",
+      priority: 0.9,
+    });
+  }
+
+  sitemap.toXML((err, xml) => {
+    if (err) {
+      console.log("error writing sitemap");
+      return;
+    }
+    fs.writeFileSync(`${directory}/sitemap.xml`, xml);
+    console.log("- Sitemap.xml saved!\n");
+  });
+}
+
+buildRobotsTxt();
+buildSitemap();
