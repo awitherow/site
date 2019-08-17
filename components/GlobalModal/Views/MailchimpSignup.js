76e3386d981124cbd08a9ae2cf84fdfad163b41a
awitherow <au.witherow@gmail.com>
2019-08-16 14:00:42 -1000

    working globalModal using object fetch contents

---
+++ components/GlobalModal/Views/MailchimpSignup.js
@@ -0,0 +1,8 @@
+export default {
+  title: "High Vibes in your Inbox, Weekly",
+  body: "Our site is currently under construction",
+  noStr: "Maybe Later",
+  yesFn: () => {},
+  yesStr: "I'm in!",
+  href: "https://mailchi.mp/a0c8d95ccbd3/highvibes-newsletter",
+};
---
+++ components/GlobalModal/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useGlobal } from "reactn";
+import { Modal, Button } from "react-bootstrap";
+
+import Views from "./Views";
+
+export default function GlobalModal() {
+  const [modal, setModal] = useGlobal("modal");
+  const handleClose = () => setModal(null);
+  const CurrentModal = Views[modal];
+  let children = null;
+
+  if (CurrentModal != null) {
+    const { title, body, href, yesStr, noStr, yesFn } = Views[modal];
+    const interaction = {};
+
+    if (href) {
+      interaction["href"] = href;
+      interaction["target"] = "_blank";
+    }
+
+    if (yesFn) {
+      interaction["onClick"] = props => yesFn(props);
+    }
+
+    console.log(interaction);
+
+    children = (
+      <>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>{body}</Modal.Body>
+
+        <Modal.Footer>
+          <Button onClick={() => setModal(null)} variant="secondary">
+            {noStr}
+          </Button>
+          <Button {...interaction} variant="primary">
+            {yesStr}
+          </Button>
+        </Modal.Footer>
+      </>
+    );
+  }
+
+  return (
+    <Modal size="lg" centered show={children != null} onHide={handleClose}>
+      {children}
+    </Modal>
+  );
+}
