c3c3d379920137232b883f69a016dafd8ffdbd56
awitherow <au.witherow@gmail.com>
2019-12-29 13:04:45 -0500

    completely r emove google adsense

--- pages/_document.js
+++ pages/_document.js
@@ -19,10 +19,6 @@ class MyDocument extends Document {
             name="fo-verify"
             content="4594dec5-0667-40cc-8974-49bff93c02e3"
           />
-          <script
-            data-ad-client="ca-pub-2149528894778035"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
