9a5f4ec7365ca8e75ce4485da5f8ec51a95fed10
awitherow <au.witherow@gmail.com>
2020-03-31 12:30:05 -1000

    add empowered essentials link

---
+++ pages/empowered-essentials.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Router from "next/router";
+
+export default class extends React.Component {
+    static async getInitialProps({ res }) {
+        if (res) {
+            res.writeHead(302, {
+                Location:
+                    "https://welcome.ly/p/wcDuqtokajQBcVj",
+            });
+            res.end();
+        } else {
+            Router.push("/");
+        }
+        return {};
+    }
+}
