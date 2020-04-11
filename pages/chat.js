af15e3d16dd23e87d984befd90a3346d41eff6a8
awitherow <au.witherow@gmail.com>
2020-04-10 16:05:37 -1000

    further trim unused pages

--- pages/chat.js
+++ pages/chat.js
@@ -5,7 +5,7 @@ export default class extends React.Component {
   static async getInitialProps({ res }) {
     if (res) {
       res.writeHead(302, {
-        Location: "https://calendly.com/awitherow/chat",
+        Location: "https://calendly.com/awitherow/",
       });
       res.end();
     } else {
