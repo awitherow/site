3d2ffcaec904e8854fc027bdc34db9250b68df2b
awitherow <au.witherow@gmail.com>
2020-08-11 14:32:20 -0400

    use different node version supported

--- pages/get-started.tsx
+++ pages/get-started.tsx
@@ -20,9 +20,10 @@ const linkCategories = [
       {
         href: "https://forms.gle/xrm2hNBxdzNysEaw9",
         title: "Schedule In Person or Online Private Lessons",
-        subtitle: "Currently offering a variety of private options in Oahu, online worldwide."
-      }
-    ]
+        subtitle:
+          "Currently offering a variety of private options in Oahu, online worldwide.",
+      },
+    ],
   },
   {
     name: "#essentialoilswork",
@@ -30,20 +31,23 @@ const linkCategories = [
       {
         href: "https://calendly.com/awitherow/wellness",
         title: "What Oils are Right for Me?",
-        subtitle: "Get FREE personalised recommendations on Natural Solutions with Essential Oils.",
+        subtitle:
+          "Get FREE personalised recommendations on Natural Solutions with Essential Oils.",
       },
       {
         href: "https://welcome.ly/p/wcDuqtokajQBcVj",
-        title: "Free Course: Empowered Living with Essential Oils",
-        subtitle: "Understand the Simple Science and Power of Purity of dōTERRAs Essential Oils",
+        title: "Empowered Living w/ Essential Oils",
+        subtitle:
+          "FREE COURSE: Understanding the Simple Science and Power of Purity of dōTERRAs Essential Oils",
       },
       {
         href:
           "https://www.doterra.com/US/en/p/healthy-habits-enrollment-kit?OwnerID=8122444",
         title: "Get Started Today with Healthy habits",
-        subtitle: "Purchase as as Wholesale Customer and get this healthy habits bundle 25% off!",
+        subtitle:
+          "Purchase as as Wholesale Customer and get this healthy habits bundle 25% off!",
       },
-    ]
+    ],
   },
   {
     name: "#ENTREPRENEURSHIP",
@@ -51,16 +55,17 @@ const linkCategories = [
       {
         href: "https://calendly.com/awitherow/coaching-clarity-session",
         title: "Schedule a Clarity Coaching Call",
-        subtitle: "Got an Idea you wanna bring into Reality? Let's get some clarity here!",
+        subtitle:
+          "Got an Idea you wanna bring into Reality? Let's get some clarity here!",
       },
       {
         href:
           "https://www.youtube.com/watch?v=yAjLJA647-w&list=PL2IhiQCs6lsjX5GDKLKCACLzNiUdLCU-k",
         title: "Agile Business Solutions Playlist",
         subtitle: "Looking for free, simple tools for your small business?",
-      }
-    ]
-  }
+      },
+    ],
+  },
 ];
 function GetStartePage({ seo }) {
   return (
@@ -72,7 +77,11 @@ function GetStartePage({ seo }) {
         <SectionHeader
           title="Tune Up Your Vibration"
           subtitle={
-            <span>Looking to get started with living a higher vibrational lifestyle? Check out these quick resources! Otherwise <Link href="/">Browse Around</Link> :)</span>
+            <span>
+              Looking to get started with living a higher vibrational lifestyle?
+              Check out these quick resources! Otherwise{" "}
+              <Link href="/">Browse Around</Link> :)
+            </span>
           }
         />
         {linkCategories.map(({ name, links }, i) => (
@@ -82,7 +91,9 @@ function GetStartePage({ seo }) {
               {links.map(({ href, title, subtitle }, i) => (
                 <a key={i} href={href} className="btn btn-success">
                   <p className="title">{title}</p>
-                  <p className="subtitle"><small>{subtitle}</small></p>
+                  <p className="subtitle">
+                    <small>{subtitle}</small>
+                  </p>
                 </a>
               ))}
             </div>
@@ -98,8 +109,8 @@ GetStartePage.getInitialProps = async ({ query }) => {
     seo: {
       title: "HIGH VIB.ES Health and Wellness | Tune Up Your Vibration",
       description:
-        "HIGH VIB.ES Health and Wellness. We promote a holistic approach to living a happy and healthy lifestyle by offering quality insights, wisdom, theory and application focused on empowering you Live Your Dream Life at its Highest Vibration. Tune in with us today and start living a life without regrets."
-    }
+        "HIGH VIB.ES Health and Wellness. We promote a holistic approach to living a happy and healthy lifestyle by offering quality insights, wisdom, theory and application focused on empowering you Live Your Dream Life at its Highest Vibration. Tune in with us today and start living a life without regrets.",
+    },
   };
 };
 
